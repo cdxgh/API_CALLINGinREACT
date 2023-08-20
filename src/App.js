0916cc8babde46a5356f6ba61175de9e598fe96b
AbdulQuadir <96329368+cdxgh@users.noreply.github.com>
2023-08-20 13:38:07 +0000

    Update 3 files

--- src/App.js
+++ src/App.js
@@ -1,18 +1,31 @@
 import "./styles.css";
 
 import { useState, useEffect } from "react";
-import { getPosts } from "./api/index";
+import { getPosts, getRandomUser } from "./api/index";
+import UserCard from "./Components/UserCard";
 
 function App() {
   const [data, setData] = useState(null);
+  const [userData, setUserData] = useState(null);
   useEffect(() => {
     getPosts().then((posts) => setData(posts));
   }, []);
+  useEffect(()=>{
+    getRandomUser().then((user)=> setUserData(user.results[0]));
+  },[]);
+
+  const refresh = ()=>{
+    getRandomUser().then((user)=> setUserData(user.results[0]));
+  };
+  
   return (
     <div className="App">
+      {userData && <UserCard data = {userData}/>}
+      <button onClick={refresh}>Refresh User</button>
       {data ? data.map((e) => <li>{e.title}</li>) : <p> NO DATA </p>}
     </div>
   );
+  
 }
 
 export default App;
